eea85371b025953a6f9feee5b59167bd840e6f9b
Hammed Waheed <worldwid01@gmail.com>
2022-09-08 14:44:39 +0200

    customImage-Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+export default function Footer() {
+	return (
+		<div className="footer container">
+			<div className="footer-section">
+				<p className="title">FoodiesExpress</p>
+				<p>
+					FoodiesExpress is a place where you can please your soul and tummy
+					with delicious food recepies of all cuisine. And our service is
+					absolutely free.
+				</p>
+			</div>
+
+			<div className="footer-section">
+				<p className="title">Contact Us</p>
+				<p>FoodiesExpress@gmail.com</p>
+				<p>
+					<a href="https://github.com/Hammed-Waheed/Our-React-Project">
+						GitHub
+					</a>
+				</p>
+				<p>
+					<a href="https://www.linkedin.com/">Linkedin</a>
+				</p>
+			</div>
+		</div>
+	);
+}
