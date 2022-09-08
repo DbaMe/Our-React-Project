350eb3d97d6c8823a685c18d160c61c13e28917d
Hammed Waheed <worldwid01@gmail.com>
2022-09-08 15:50:38 +0200

    pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,13 @@ import ImproveSkills from "../components/ImproveSkills";
 import QouteSection from "../components/QuoteSection";
 import ChefsSection from "../components/ChefsSection";
 
-
-
-export default function Home(){
-    return (
-        <div>
-            <HeroSection />
-            <ImproveSkills />
-            <QouteSection />
-            <ChefsSection />
-        
-        </div>
-    )
+export default function Home() {
+	return (
+		<div>
+			<HeroSection />
+			<ImproveSkills />
+			<QouteSection />
+			<ChefsSection />
+		</div>
+	);
 }
